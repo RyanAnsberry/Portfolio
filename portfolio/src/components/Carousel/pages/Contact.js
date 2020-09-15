29d4f3140f681899c4625d6b0287779e44bc1df1
Ryan Ansberry <ansman92@gmail.com>
2020-09-15 13:40:22 -0700

    contact page changes

--- portfolio/src/components/Carousel/pages/Contact.js
+++ portfolio/src/components/Carousel/pages/Contact.js
@@ -1,25 +1,65 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { FaGithub, FaLinkedin } from 'react-icons/fa';
 import './Page.css'
 
-export default function Contact() {
-    return (
-        <section className="Contact page">
-            <div className="contact-page-container">
-                <h1 className="contact-header">CONTACT</h1>
-                <div className="contact-email-prompt-container">
-                    <h3 className="contact-email-header">Email: <span className="contact-my-email">ryanansberry@gmail.com</span></h3>
-                    <form className="contact-email-form">
-                        <input className="contact-email-input" placeholder="example@gmail.com..."></input>
-                        <input className="contact-email-body"></input>
-                        <button type="submit" className="contact-email-submit">Submit</button>
+class Contact extends Component {
+    constructor() {
+        super();
+        this.state = {
+            name: '',
+            email: '',
+            message: ''
+        }
+    }
+
+    handleChange = e => {
+        e.preventDefault();
+		this.setState({
+			[e.target.name]: e.target.value,
+		})
+    }
+
+    handleSubmit = e => {
+        e.preventDefault();
+        console.log(this.state)
+    }
+
+    render() {
+        return (
+            <section className="Contact page">
+                <div className="contact-page-container">
+
+                    <h1 className="contact-header">CONTACT</h1>
+
+                    <form className="contact-form">
+
+                        <div className="contact-form-group contact-name">
+                            <label className="contact-form-label">Name</label>
+                            <input name="name" type="text" className="contact-form-input" value={this.state.name} onChange={this.handleChange} />
+                        </div>
+
+                        <div className="contact-form-group contact-email">
+                            <label className="contact-form-label">Email</label>
+                            <input name="email" type="text" className="contact-form-input" value={this.state.email} onChange={this.handleChange} />
+                        </div>
+
+                        <div className="contact-form-group contact-message">
+                            <label className="contact-form-label">Message</label>
+                            <textarea name="message" type="text" className="contact-form-textarea" value={this.state.message} onChange={this.handleChange} />
+                        </div>
+
+                        <button type="submit" className="contact-form-submit-button">Submit</button>
+
                     </form>
+
+                    <div className="contact-links-container">
+                        <a href="https://github.com/RyanAnsberry" target="_blank" rel="noopener noreferrer" className="contact-link github"><FaGithub/></a>
+                        <a href="https://www.linkedin.com/in/ryan-ansberry-11a55415a/" target="_blank" rel="noopener noreferrer" className="contact-link linkedin"><FaLinkedin/></a>
+                    </div>
                 </div>
-                <div className="contact-links-container">
-                    <a href="https://github.com/RyanAnsberry" target="_blank" rel="noopener noreferrer" className="contact-link github"><FaGithub/></a>
-                    <a href="https://www.linkedin.com/in/ryan-ansberry-11a55415a/" target="_blank" rel="noopener noreferrer" className="contact-link linkedin"><FaLinkedin/></a>
-                </div>
-            </div>
-        </section>
-    )
+            </section>
+        )
+    }
 }
+
+export default Contact;
